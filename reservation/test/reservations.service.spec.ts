047b71b62b80a8690b6e2aff7c2307c8e565f963
amyepes <amyepes@uninorte.edu.co>
2025-10-15 22:56:35 -0500

    added unit and integration tests
    sketch of the unit and integration tests along with a folder for the integration testing created

---
+++ reservation/test/reservations.service.spec.ts
@@ -0,0 +1,17 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { ReservationsService } from '../src/application/service/reservations.service';
+
+describe('ReservationsService', () => {
+  let service: ReservationsService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [ReservationsService],
+    }).compile();
+    service = module.get<ReservationsService>(ReservationsService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+});
